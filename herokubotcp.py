0f2a1cc3c26992584c0f6e67d5bce4840098dcbd
Addic7edBoy <tratatamaxim@gmail.com>
2018-11-15 13:37:15 +0700

    gb_test

--- herokubotcp.py
+++ herokubotcp.py
@@ -3,15 +3,30 @@ import os
 from flask import Flask, request
 import logging
 import config
-
+import time
 bot = telebot.TeleBot(config.token)
 
 # Здесь пишем наши хэндлеры
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def gb_strong(message):
+    if message.text == 'Хэй бот скинь свой любимый трек':
+        f = open('photo2.jpg', 'rb')
+        v = open('music/GB_squad.ogg', 'rb')
+        bot.send_photo(message.chat.id, f)
+        bot.send_voice(message.chat.id, v)
+    if message.text == 'Ты ебанутый?':
+        time.sleep(0.2)
+        bot.send_message(message.chat.id, 'Закройся лох это шедевр')
+    if message.text == 'Я создал монстра':
+        time.sleep(0.3)
+        bot.send_message(message.chat.id, 'Я тебя щас забаню кожаный ублюдок')
+
+
+#@bot.message_handler(func=lambda message: True, content_types=['text'])
+#def echo_message(message):
+#    bot.reply_to(message, message.text)
 
 if "HEROKU" in list(os.environ.keys()):
     logger = telebot.logger
